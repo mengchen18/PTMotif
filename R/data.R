fc08503baa7325b1d449de8e65035a818322e27f
ga52cib <chen.meng@tum.de>
2018-02-04 01:05:41 +0100

    document example data

---
+++ R/data.R
@@ -0,0 +1,11 @@
+#' Example dataset for motif analysis
+#' 
+#' @docType data
+#' @name motifExampleData
+#' @usage data(diamonds)
+#' @format A \code{list} with two elements:
+#' \describe{
+#'   \item{foreground}{foreground sequences}
+#'   \item{background}{background sequences}
+#' }
+NULL
